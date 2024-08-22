677316588643c8167fb2bd98e77ec607b3c37e5f
Husssein007 <husseinlimem8@gmail.com>
2024-08-22 16:53:23 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React from 'react';
+import PlayersList from './components/PlayersList';
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center mt-4">Player Profiles</h1>
+      <PlayersList />
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ players.js
@@ -0,0 +1,39 @@
+
+
+
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami CF",
+      nationality: "Argentinian",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://imageio.forbes.com/specials-images/imageserve/65d11181840909c74ae8099a/SOCCER--FEB-15-MLS-Preseason-Inter-Miami-CF-vs-Newell-s-Old-Boys/960x0.jpg?format=jpg&width=960"
+    },
+    {
+      name: "LeBron James",
+      team: "Los Angeles Lakers",
+      nationality: "American",
+      jerseyNumber: 23,
+      age: 39,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a7/LeBron_James_Lakers.jpg"
+    },
+    {
+      name: "Serena Williams",
+      team: "Usa",
+      nationality: "American",
+      jerseyNumber: 1,
+      age: 42,
+      imageUrl: "https://media2.ledevoir.com/images_galerie/nwd_743422_581842/image.jpg"
+    },
+    {
+      name: "Usain Bolt",
+      team: "Jamaica",
+      nationality: "jamaïcain",
+      jerseyNumber: 958,
+      age: 38,
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Usain_Bolt_Rio_100m_final_2016i-cr.jpg/1200px-Usain_Bolt_Rio_100m_final_2016i-cr.jpg"
+    }
+  ];
+  
+  export default players;
